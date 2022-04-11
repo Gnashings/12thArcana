f0aba64ca8408c05cf71765a6ee4584563230f21
Nash Roy <nashroy11@gmail.com>
2022-04-10 20:45:41 -0700

    update

--- Assets/Scripts/PlayTimeStats.cs
+++ Assets/Scripts/PlayTimeStats.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayTimeStats : MonoBehaviour
 {
-    public Text timeText;
+    public TextMeshProUGUI timeText;
+    public TextMeshProUGUI levelsPlayed;
     // Start is called before the first frame update
     void Start()
     {
+
         timeText.text = LevelProgress.timePlayed.ToString() + " Seconds!";
+        if (LevelProgress.timePlayed == 0)
+        {
+            timeText.text = "0 " + "Seconds";
+        }
+        levelsPlayed.text = LevelProgress.levelCount.ToString() + "Levels";
+        if (LevelProgress.levelCount == 0)
+        {
+            levelsPlayed.text = "0 " + "Levels";
+        }
     }
 
     // Update is called once per frame
